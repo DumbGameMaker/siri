28ffc9e1bb9519c65bd2f8ba8f51e1e6b443bde2
useCamelCase <qwertyytofficialyt@gmail.com>
2022-04-24 09:04:25 -0700

    8-perms

--- commands/game.rps.js
+++ commands/game.rps.js
@@ -17,15 +17,23 @@ module.exports = {
     ),
   async execute(interaction, client) {
     let chosen = interaction.options.getString("item");
+    if (chosen == "god")
+      interaction.reply("i choose yo mama, who's so fat she crushes god");
     let bot = ["rock", "paper", "scissors"][crypto.randomInt(0, 2)];
-    if (chosen == bot) return interaction.reply("draw");
-    if (chosen == "rock" && bot == "scissors") return interaction.reply("win");
-    if (chosen == "rock" && bot == "paper") return interaction.reply("lose");
-    if (chosen == "paper" && bot == "rock") return interaction.reply("win");
+    if (chosen == bot) return interaction.reply(`I choose ${bot}; i win`);
+    if (chosen == "rock" && bot == "scissors")
+      return interaction.reply("i chose scissors; you win");
+    if (chosen == "rock" && bot == "paper")
+      return interaction.reply("i chose paper; you lose");
+    if (chosen == "paper" && bot == "rock")
+      return interaction.reply("i chose rock; you win");
     if (chosen == "paper" && bot == "scissors")
-      return interaction.reply("lose");
-    if (chosen == "scissors" && bot == "paper") return interaction.reply("win");
-    if (chosen == "scissors" && bot == "rock") return interaction.reply("lose");
+      return interaction.reply("i chose scissors; you lose");
+    if (chosen == "scissors" && bot == "paper")
+      return interaction.reply("i chose paper; you win");
+    if (chosen == "scissors" && bot == "rock")
+      return interaction.reply("i chose rock; you lose");
+
     return interaction.reply("you lose hahahahahahahhahahahhahahahahhahahah");
   },
 };
