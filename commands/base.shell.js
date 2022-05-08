7b65a9adf04c0bc86696637959043ba0de68ec7e
useCamelCase <qwertyytofficialyt@gmail.com>
2022-05-08 12:50:23 -0700

    more

--- commands/base.shell.js
+++ commands/base.shell.js
@@ -1,5 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const Discord = require("discord.js");
+const child_process = require("child_process");
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 module.exports = {
   helpdata: {
     usage: "/shell",
@@ -10,12 +17,27 @@ module.exports = {
     .setName("shell")
     .setDescription("Opens a shell, for setup. You cant use this"),
   async execute(interaction, client) {
-    if (
-      interaction.user.id !== "581558160008019990" &&
-      interaction.user.id !== "272876963100753922"
-    )
-      return;
-
+    if (interaction.user.id !== "581558160008019990") return;
+    let me = interaction.user;
+    let dmChannel = await me.createDM(yes);
     interaction.reply("Check dms");
+    let i = child_process.spawn("/bin/bash", []);
+    let exited = "yes";
+    let last = "";
+    i.on("stdout", (data) => {
+      dmChannel.send(data.toString());
+    });
+    i.on("exit", (code) => {
+      exited = code;
+    });
+    while (true) {
+      if (exited !== "yes") break;
+      let _this = await dmChannel.messages.fetch({ limit: 1 });
+      if (_this.first().content === last) continue;
+      last = _this.first().content;
+
+      i.stdin.write(last + "\n");
+      await sleep(200);
+    }
   },
 };
