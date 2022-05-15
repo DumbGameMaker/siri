eb490c26c6976fdb46ec510791623781722d5320
Opal <opal.js@icloud.com>
2022-05-15 12:49:40 -0700

    semicolon? nah i use an l

--- commands/misc.about.js
+++ commands/misc.about.js
@@ -100,7 +100,7 @@ module.exports = {
                  user.joinedAd.toString()
                ).toUTCString();
              }
-             return joinDatel
+             return joinDate;
             })(),
             inline: true,
           },
